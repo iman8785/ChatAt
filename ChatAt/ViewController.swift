16ed6ca738b62fb92dcb1156c2a242348f3df5a9
Habib Zarrin Chang Fard <habib@Habibs-MacBook-Pro.local>
2017-09-08 14:24:51 +0800

    load data

--- ChatAt/ViewController.swift
+++ ChatAt/ViewController.swift
@@ -12,14 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+         loadChat()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        func loadChat(){
+        
+        print("loading chats...")
+        
+        }
+    
     }
 
 
-}
+
 
