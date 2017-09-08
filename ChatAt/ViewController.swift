8810328505cd8affd378a99ba472be54a27dec82
Habib Zarrin Chang Fard <habib@Habibs-MacBook-Pro.local>
2017-09-08 14:27:38 +0800

    add load chat room function

--- ChatAt/ViewController.swift
+++ ChatAt/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   loadchatroomdetails()
+    }
+    
+    func loadchatroomdetails(){
+    
+    print("load chat room details")
+    
     }
 
     override func didReceiveMemoryWarning() {
